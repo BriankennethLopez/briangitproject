dee0064fbbae64082565c66974d7674085c86b0a
BriankennethLopez <60036414+BriankennethLopez@users.noreply.github.com>
2020-02-14 01:42:04 -0800

    new

---
+++ app/src/main/java/edu/ucucite/briangithub/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ucucite.briangithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
